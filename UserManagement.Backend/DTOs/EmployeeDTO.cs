986d6683541d9b2817a790dd38f8b2258a525f67
Sanjay Senthilkumar <183524427+JaySanSen@users.noreply.github.com>
2025-01-21 03:03:31 +0530

    added base DTO employee class

---
+++ UserManagement.Backend/DTOs/EmployeeDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace UserManagement.Backend.DTOs
+{
+  public class EmployeeDTO
+  {
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string? Email { get; set; }
+    public long PhoneNumber { get; set; }
+
+  }
+}
